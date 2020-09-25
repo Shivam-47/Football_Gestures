7d89815f9e26028e57dc415200b2752742a2f390
uNaSsIgNEd <49410579+Shivam-47@users.noreply.github.com>
2020-09-26 03:09:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,34 @@
+import math
 import cv2
 import numpy as np
-import math
+
 
 cap = cv2.VideoCapture(0)
 
 while (cap.isOpened()):
 
-    # try:  # an error comes if it does not find anything in window as it cannot find contour of max area
-    # therefore this try error statement
-
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
     kernel = np.ones((3, 3), np.uint8)
 
-    # define region of interest
+    # ROI
     roi = frame[100:400, 100:400]
 
     cv2.rectangle(frame, (100, 100), (400, 400), (0, 255, 0), 0)
     hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
 
-    # define range of skin color in HSV
+    # range of color in HSV
     lower_skin = np.array([0, 20, 70], dtype=np.uint8)
     upper_skin = np.array([20, 255, 255], dtype=np.uint8)
-
-    # extract skin colur imagw
     mask = cv2.inRange(hsv, lower_skin, upper_skin)
 
-    # extrapolate the hand to fill dark spots within
+    #dilate
     mask = cv2.dilate(mask, kernel, iterations=4)
-
     # blur the image
     mask = cv2.GaussianBlur(mask, (5, 5), 100)
-
     # find contours
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    #  print("Test")
-    # find contour of max area(hand)
     cnt = max(contours, key=lambda x: cv2.contourArea(x))
-
-    # approx the contour a little
     epsilon = 0.0005 * cv2.arcLength(cnt, True)
     approx = cv2.approxPolyDP(cnt, epsilon, True)
 
@@ -68,20 +57,20 @@ while (cap.isOpened()):
         far = tuple(approx[f][0])
         pt = (100, 180)
 
-        # find length of all sides of triangle
+        # length of all sides of triangle
         a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
         b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
         c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
         s = (a + b + c) / 2
         ar = math.sqrt(s * (s - a) * (s - b) * (s - c))
 
-        # distance between point and convex hull
+        # distance between point and hull
         d = (2 * ar) / a
 
-        # apply cosine rule here
+        # apply cos rule
         angle = math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)) * 57
 
-        # ignore angles > 90 and ignore points very close to convex hull(they generally come due to noise)
+        # ignore angles > 90 and ignore points very close to convex hull
         if angle <= 90 and d > 30:
             l += 1
             cv2.circle(roi, far, 3, [255, 0, 0], -1)
@@ -94,7 +83,7 @@ while (cap.isOpened()):
     print("l " + str(l))
     print("area ratio " + str(arearatio))
 
-    # print corresponding gestures which are in their ranges
+    # print gestures which are in their ranges
     font = cv2.FONT_HERSHEY_SIMPLEX
     if l == 1:
         if areacnt < 2000:
